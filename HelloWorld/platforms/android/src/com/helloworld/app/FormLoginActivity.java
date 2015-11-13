91b1ad00bcfab7fa308bf9e6ea21dee7432a7fb7
raizeta <rai_zet@hotmail.com>
2015-11-13 15:24:47 +0700

    Add First Project

---
+++ HelloWorld/platforms/android/src/com/helloworld/app/FormLoginActivity.java
@@ -0,0 +1,18 @@
+package com.helloworld.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class FormLoginActivity extends Activity 
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+	}
+
+}
