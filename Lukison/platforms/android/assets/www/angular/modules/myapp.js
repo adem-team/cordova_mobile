9e46f124661d909aacf1459ab29d4b4bfc10aa05
projectku <piter@lipat.co.id>
2015-12-15 13:04:17 +0700

    add barcode scanner

---
+++ Lukison/platforms/android/assets/www/angular/modules/myapp.js
@@ -0,0 +1,4 @@
+var myAppModule 	= angular.module('myApp',
+								['ngRoute','angularSpinner','ui.bootstrap',
+								 'ng-fusioncharts','naif.base64','monospaced.qrcode','ngCordova']);
+
---
+++ Lukison/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-splashscreen": "3.0.0",
+    "phonegap-plugin-barcodescanner": "4.0.1"
+}
+// BOTTOM OF METADATA
+});
